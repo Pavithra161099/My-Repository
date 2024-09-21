2d418e6803175c99528a1ed959565f585c951475
Pavithra161099 <rpavithra161099@gmail.com>
2024-09-22 02:51:51 +0530

    first commit

--- src/test/java/demo/Demotest/test1.java
+++ src/test/java/demo/Demotest/test1.java
@@ -1,5 +1,7 @@
 package demo.Demotest;
 
+import java.time.Duration;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -7,12 +9,15 @@ public class test1 {
 
 	@Test
 	public void test1() {
-		System.setProperty("websriver.chrome.driver", "//Users//vijaya.bonthu//Downloads//chromedriver-mac-x64//chromedriver");
+		System.setProperty("websriver.chrome.driver",
+				"//Users//vijaya.bonthu//Downloads//chromedriver-mac-x64//chromedriver");
 
 		ChromeDriver driver = new ChromeDriver();
-		
+
 		driver.get("https://www.google.com");
-	
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 	}
 }
